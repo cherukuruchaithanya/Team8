9be4b5af0a1247deb2c58ae56b7ecf9e50578b33
Kurapati Midhun Kumar <S531502@nwmissouri.edu>
2019-03-03 15:01:16 -0600

    "model of Studenttable"

---
+++ Team8/Models/Studenttable.cs
@@ -0,0 +1,11 @@
+using System;
+using System.Collections.Generic;
+namespace Team8.Models
+{
+    public class Studenttable
+    {
+        public int StudnetID { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+            }
+}
