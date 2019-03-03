da26b33162a849486612c3c0717ba5e8faec4c4f
Kurapati Midhun Kumar <S531502@nwmissouri.edu>
2019-03-03 14:59:58 -0600

    "Requirement model"

---
+++ Team8/Models/Requirement.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+namespace Team8.Models
+{
+
+    public class Requirement
+{
+    public int RequirementID { get; set; }
+    public string Requirement { get; set; }
+    public string RequirementName { get; set; }
+}
+}
