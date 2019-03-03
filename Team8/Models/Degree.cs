847040e1996d7c78df1760bafa54cdd0302f23f0
rn <s525956@nwmissouri.edu>
2019-03-03 15:45:00 -0600

    Updated

---
+++ Team8/Models/Degree.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+namespace Team8.Models
+{
+
+    public class Requirement
+{
+    public int DegreeId { get; set; }
+    public string Degree { get; set; }
+    public string DegreeName { get; set; }
+}
+}
---
+++ Team8/Models/DegreeplanTermRequirement.cs
@@ -0,0 +1,26 @@
+using System;
+
+using System.Collections.Generic;
+
+namespace Team8.Models
+
+{
+
+    public class DegreePlanTermRequirement
+
+    {
+
+        public int DegreePlanTermRequirementID { get; set; }
+
+        public int DegreePlanID { get; set; }
+
+        public int TermID { get; set; }
+
+        public int RequirementID { get; set; }
+
+ 
+
+    }
+
+}
+
