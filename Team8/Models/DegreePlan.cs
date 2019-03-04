7f00566433f618654ea4bf7f1a2840ebb0590b1a
Girish Guntuku <S531369@nwmissouri.edu>
2019-03-04 01:32:44 -0600

    added degree and degree plan

---
+++ Team8/Models/DegreePlan.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+
+namespace Team8.Models
+{
+    public class StudnetTerm
+    {
+        public int DegreePlanId { get; set; }
+        public int DegreeID { get; set; }
+        public int StudentId { get; set; }
+        public string DegreePlan { get; set; }
+        public string DegreePlanName { get; set; }
+
+
+    }
+}
