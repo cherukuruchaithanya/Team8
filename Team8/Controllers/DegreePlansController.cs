7f2f8ebf6bc75f591ea99f04d02b91436b8b409a
Girish Guntuku <S531369@nwmissouri.edu>
2019-03-22 14:01:38 -0500

    "beta"

--- Team8/Controllers/DegreePlansController.cs
+++ Team8/Controllers/DegreePlansController.cs
@@ -54,9 +54,7 @@ namespace Team8.Controllers
             return View();
         }
 
-        // POST: DegreePlans/Create
-        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
-        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+       
         [HttpPost]
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> Create([Bind("DegreePlanId,DegreeId,StudentId,DegreePlans,DegreePlanName,SortOrder")] DegreePlan degreePlan)
